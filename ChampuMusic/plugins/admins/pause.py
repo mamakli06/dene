b74f2593937b3515744952a9e301b983259cc38e
mamakli06 <83599873+mamakli06@users.noreply.github.com>
2025-03-10 10:36:31 +0300

    Update pause.py

--- ChampuMusic/plugins/admins/pause.py
+++ ChampuMusic/plugins/admins/pause.py
@@ -8,7 +8,7 @@ from ChampuMusic.utils.database import is_music_playing, music_off
 from ChampuMusic.utils.decorators import AdminRightsCheck
 
 
-@app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["pause", "durdur","cpause"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
